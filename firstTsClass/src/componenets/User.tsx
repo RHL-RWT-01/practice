325074e9ba35cccd6be9bdc56624ec0621cb7b61
Rahul Rawat <rahulrawat4060@gmail.com>
2025-01-31 15:46:04 +0530

    Add focus management to User component with useRef

--- firstTsClass/src/componenets/User.tsx
+++ firstTsClass/src/componenets/User.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 type LoginInfo = {
     userId: string,
     password: string
@@ -6,18 +6,20 @@ type LoginInfo = {
 const details: any = [];
 
 function User() {
-
+    const idRef=useRef(null);
+   
     const [loginInfo, setLoginInfo] = useState<LoginInfo>({ userId: '', password: '' });
 
     const handleClick = () => {
         details.push(loginInfo);
         setLoginInfo({ userId: '', password: '' });
+        idRef.current.focus();
     }
 
     return (
         <>
-            <input onChange={(e) => setLoginInfo({ ...loginInfo, userId: e.target.value })} type="text" placeholder="Enter User ID" />
-            <input onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })}
+            <input ref={idRef} onChange={(e) => setLoginInfo({ ...loginInfo, userId: e.target.value })} type="text" placeholder="Enter User ID" />
+            <input  onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })}
                 type="password" placeholder="Enter Password" />
             <button onClick={() => handleClick()} >Save</button>
             <ul>
