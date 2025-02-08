7be7f6ebf5564f62bf1d82fb3b5b854a0d70fbf7
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-08 23:22:25 +0530

    Refactor action creators in Redux to improve readability and maintainability

--- footballMatch/src/redux/Actions.jsx
+++ footballMatch/src/redux/Actions.jsx
@@ -4,11 +4,11 @@ export const getMatches=()=>{
 export const getMatchSuccess=(data)=>{
     return {type: "GET_MATCHES_SUCCESS", payload: data};
 }
+
+
 export const getMatchFailure=()=>{
     return {type: "GET_MATCHES_FAILURE"};
 }
 export const filterByTeamName=()=>{
     return {type: "FILTER_BY_TEAM_NAME"};
 }
-
-
