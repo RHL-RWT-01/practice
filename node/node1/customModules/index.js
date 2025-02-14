f66ab56b5910fd1b5e09f89dd7066b69ccbe7037
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-14 15:00:47 +0530

    Initialize Node.js project with basic arithmetic operations and external module integration

---
+++ node/node1/customModules/index.js
@@ -0,0 +1,10 @@
+const add =(a,b) =>{ return a+b; };
+const subtract = (a,b) => { return a-b; };
+const multiply = (a,b) => { console.log(a*b) ; };
+const divide = (a,b) => { return a/b; };
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+};
---
+++ node/node1/customModules/ops.js
@@ -0,0 +1,9 @@
+const {add,subtract,divide,multiply} = require('./index.js');
+
+console.log(add(1,2));
+
+console.log(subtract(5,3));
+
+console.log(divide(10,2));
+
+console.log(multiply(3,4));
