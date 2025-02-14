f69bcc24d1d2d8389a3f9204668cd3c688c6415d
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-14 15:42:35 +0530

    Add file operations for creating, reading, and updating text files in Node.js

---
+++ node/node1/internalModules/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function create1(){
+    fs.writeFile('./masai.text','Hi This is Rahul Rawat , Learning Node',(e)=>{
+        if(e) {
+            console.error('Error:', e);
+            return;
+        };
+        console.log('File Created');
+    })
+}
+function create2(){
+    fs.writeFileSync('./stl.text','Hi This is Rahul Rawat')
+        console.log('File Created');
+    
+}
+
+function read(){
+    fs.readFile('./masai.text', 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
+}
+
+function update(){
+    fs.appendFile('./masai.text',', I am learning Backend', (err) => {
+        if (err) throw err;
+        console.log('Updated');
+    });
+}
+
+create1();
+console.log("Rahul")
+create2();
+read();
+update();
