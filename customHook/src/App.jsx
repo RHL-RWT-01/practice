1a7c4bad2482ed114aaa8b73423b1594cf45f9c6
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-15 20:53:26 +0530

    Add useTimer hook

--- customHook/src/App.jsx
+++ customHook/src/App.jsx
@@ -1,14 +1,53 @@
 import React, { useState } from "react";
 import useLocalStorage from "./components/UseLocalStorage";
 import useToggleItems from "./components/useToggleItems ";
+import useTimer from "./components/useTimer";
 
 function App() {
   const [name, setName] = useLocalStorage("username", "Rahul");
- const arr = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
- const [idx,setIdx]=useState(0);
-  const item = useToggleItems(arr,idx);
+  const arr = [
+    "",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const [idx, setIdx] = useState(0);
+  const item = useToggleItems(arr, idx);
+  const { timer, isRunning, startTimer, stopTimer, resetTimer } = useTimer();
   return (
-    <div>
+    <div
+      style={{
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        
+      }}
+    >
       <h1>Welcome, {name}</h1>
       <input
         type="text"
@@ -16,8 +55,16 @@ function App() {
         onChange={(e) => setName(e.target.value)}
       />
       <h1>Item: {item}</h1>
-      <input onChange={(e)=>setIdx(e.target.value)} placeholder="Enter Index between 1 and 25"></input>
-      
+      <input
+        onChange={(e) => setIdx(e.target.value)}
+        placeholder="Enter Index between 1 and 25"
+      ></input>
+
+      <h1>Timer: {timer}</h1>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+      <h1>Is Running: {isRunning? "Yes" : "No"}</h1>
     </div>
   );
 }
---
+++ customHook/src/components/useTimer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useTimer() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const startTimer = () => setIsRunning(true);
+  const stopTimer = () => setIsRunning(false);
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTimer(0);
+  };
+
+  return { timer, isRunning, startTimer, stopTimer, resetTimer };
+}
+
+export default useTimer;
