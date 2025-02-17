44c80b840cb856afa2c84f3aac21dfd5ce0816c4
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-17 12:25:11 +0530

    Refactor multiplication function to return result and update ops.js to handle command line arguments for arithmetic operations

--- node/node1/customModules/ops.js
+++ node/node1/customModules/ops.js
@@ -1,9 +1,14 @@
 const {add,subtract,divide,multiply} = require('./index.js');
+const args = process.argv.slice(2); 
 
-console.log(add(1,2));
+const a=args[0];
 
-console.log(subtract(5,3));
+const b=args[1];
+console.log(a,b)
 
-console.log(divide(10,2));
 
-console.log(multiply(3,4));
+console.log(subtract(a,b));
+
+console.log(divide(a,b));
+
+console.log(multiply(a,b));
