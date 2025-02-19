0dad88ad96a2163408fe10a26c650bb181abbbf9
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-19 22:44:34 +0530

    Add Todos component with API integration and dynamic rendering

--- ApiInTsx/src/components/Todos.tsx
+++ ApiInTsx/src/components/Todos.tsx
@@ -1,11 +1,26 @@
 import { useState } from "react"
-
+type Todo = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 function Todos() {
-    const [todos, setTodos] = useState([])
-    
-  return (
-    <div>Todos</div>
-  )
+    const [todos, setTodos] = useState<Todo[]>([])
+    const handleClick = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const data = await response.json()
+        setTodos([data])
+    }
+
+    return (
+        <>
+            <h1>Todos</h1>
+            <button onClick={handleClick} >Get</button>
+            {todos.length == 0 ? <p>No todos</p> : <ul>{todos.map((todo: Todo) => <li key={todo.id}>{todo.title}
+            <input type="checkbox" checked={todo.completed} ></input></li>)}</ul>}
+        </>
+    )
 }
 
 export default Todos
