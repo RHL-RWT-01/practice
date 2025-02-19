cfd9d0ee45c922bc36c2058e1e652442eaa36ca3
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-19 22:26:47 +0530

    Implement timer component with start, reset, and stop functionality

--- counterUsingTsx/src/Counter.tsx
+++ counterUsingTsx/src/Counter.tsx
@@ -1,8 +1,35 @@
+import { useState, useRef } from "react";
 
 function Counter() {
-  return (
-    <div>Counter</div>
-  )
+    const [time, setTime] = useState(0);
+    const timer = useRef(null);
+
+    const handleClick = () => {
+        if (!timer.current) {
+            timer.current = setInterval(() => {
+                setTime((prev) => prev + 1);
+            }, 1000);
+        }
+    };
+
+    const handleReset = () => {
+        setTime(0);
+    };
+
+    const handleStop = () => {
+        clearInterval(timer.current);
+        timer.current = null;
+    };
+
+    return (
+        <>
+            <h1>Timer</h1>
+            <h2>{time}</h2>
+            <button onClick={handleClick}>Start</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleStop}>Stop</button>
+        </>
+    );
 }
 
-export default Counter
+export default Counter;
