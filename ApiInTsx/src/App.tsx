df02971dd551deea758b3f3114c62fd210669e9a
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-19 22:31:42 +0530

    API calls in TSX

---
+++ ApiInTsx/src/App.tsx
@@ -0,0 +1,14 @@
+
+import Todos from './components/Todos'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
---
+++ ApiInTsx/src/components/Todos.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function Todos() {
+    const [todos, setTodos] = useState([])
+    
+  return (
+    <div>Todos</div>
+  )
+}
+
+export default Todos
