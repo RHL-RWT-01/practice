d3950362556b25ad04c1f0c2f62c727e8129e4d4
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-22 23:24:18 +0530

    Add nodemon dependency and initial project structure

--- node/node1/httpServer/index.js
+++ node/node1/httpServer/index.js
@@ -1,6 +1,22 @@
 const http = require('http');
-
+const fs = require('fs');
+function createFile() {
+    fs.writeFile('./masai.text', 'Hi This is Rahul Rawat , Learning Node', (e) => {
+        if (e) {
+            console.error('Error:', e);
+            return;
+        };
+        return 'File Created';
+    })
+}
+function readFile() {
+    fs.readFile('./masai.text', 'utf8', (err, data) => {
+        if (err) throw err;
+        return data;
+    });
+}
 const server = http.createServer((req, res) => {
+
     if (req.url == '/' && req.method == 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({
@@ -8,12 +24,14 @@ const server = http.createServer((req, res) => {
         }));
     }
     else if (req.url == '/about' && req.method == 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
+        createFile();
+        let data = readFile();
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(JSON.stringify({
-            data: 'About Us Page',
+            data
         }));
     }
-    else{
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 Not Found API is not defind');
     }
