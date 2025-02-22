6c548c4d38d5ece8aa844b2c1c567ab742767a5f
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-22 23:27:29 +0530

    implemented reducer for reset action

--- counter/src/redux/Reducer.jsx
+++ counter/src/redux/Reducer.jsx
@@ -9,6 +9,8 @@ const countReducer = (state = initailState, action) => {
       return { ...state, count: state.count + 1 };
     case "DECREMENT":
       return { ...state, count: state.count - 1 };
+    case "RESET":
+      return { ...state, count: 0 };
     default:
       return state;
   }
