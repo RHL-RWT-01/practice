11c65777217d7693b86beffa7bf7db97cfc6f6d2
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-26 23:33:39 +0530

    Remove commented-out toggle functionality from todoReducer

--- redux/src/states/Reducers.jsx
+++ redux/src/states/Reducers.jsx
@@ -6,10 +6,6 @@ export const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [...state, { id: action.payload.id, text: action.payload.text, completed: false }];
-        // case 'TOGGLE_TODO':
-        //     return state.map(todo =>
-        //         todo.id === action.id? {...todo, completed:!todo.completed } : todo
-        //     );
         case 'DELETE_TODO':
             return state.filter(todo => todo.id!== action.id);
         default:
