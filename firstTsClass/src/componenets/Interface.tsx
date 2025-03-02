9a1b45d91633e95a19df0115d527c57790a8c887
Rahul Rawat <rahulrawat4060@gmail.com>
2025-03-02 22:47:30 +0530

    extends in tsx

--- firstTsClass/src/componenets/Interface.tsx
+++ firstTsClass/src/componenets/Interface.tsx
@@ -3,12 +3,16 @@ interface User {
     age: number,
     email: string
 }
-function Interface({ name, age, email }: User) {
+interface Employee extends User {
+    salary: number
+}
+function Interface({ name, age, email, salary}: Employee) {
     return (
         <>
             <h2>Name:{name}</h2>
             <h2>Age:{age}</h2>
             <h2>Email:{email}</h2 >
+            <h3>Salary: {salary}</h3>
         </>
     )
 }
