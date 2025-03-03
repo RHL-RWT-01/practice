f8a0237b7885895b0e4ebfbb8a247dac1fd8b26c
Rahul Rawat <rahulrawat4060@gmail.com>
2025-03-03 22:29:05 +0530

    Fetch tasks from API and display them in Task component

--- taskmanager/src/components/Task.tsx
+++ taskmanager/src/components/Task.tsx
@@ -1,20 +1,20 @@
+type TaskType = { title: string; description: string };
+type Tasks = TaskType[];
 
-function Task({tasks}) {
+function Task({ tasks }: { tasks: Tasks }) {
     return (
         <>
             <h1>Tasks</h1>
             {
-                tasks.map((task, index) => {
-                    return (
-                        <div key={index}>
-                            <h3>{task.title}</h3>
-                            <p>{task.description}</p>
-                        </div>
-                    )
-                })
+                tasks.map((task: TaskType, index: number) => (
+                    <div key={index}>
+                        <h3>{task.title}</h3>
+                        <p>{task.description}</p>
+                    </div>
+                ))
             }
         </>
-    )
+    );
 }
 
-export default Task
+export default Task;
