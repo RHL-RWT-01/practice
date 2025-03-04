6d3a9e2550d8fe7b606de3ea73b7348f76078432
Rahul Rawat <rahulrawat4060@gmail.com>
2025-03-04 22:22:58 +0530

    feedback sections and a feedback form

--- feedbackSystem/src/App.tsx
+++ feedbackSystem/src/App.tsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
+      <h1>Feedback system</h1>
+      <h2>Feedbacks</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h3>Feedback 1</h3>
+          <p>Feedback description</p>
+        </div>
+        <div>
+          <h3>Feedback 2</h3>
+          <p>Feedback description</p>
+        </div>
+        <div>
+          <h3>Feedback 3</h3>
+          <p>Feedback description</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Feedback form</h2>
+      <form>
+        <label>
+          Feedback
+          <input type="text" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
